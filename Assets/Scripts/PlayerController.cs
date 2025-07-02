380f875f06d02114c4f06d423a31172d05ae333c
DESKTOP-GIUQICH\202_Main <jungchanjoong@gmail.com>
2025-07-02 11:17:58 +0900

    컨트롤변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,40 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    private Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f; // 이동 속력
 
     void Start()
     {
         // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
-        //playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            // 위쪽 방향키 입력이 감지된 경우 z 방향 힘가기
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
 
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘가기
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
+        // 수평과 수직 축 입력 값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        Debug.Log("xInput: " + xInput);
+        float zInput = Input.GetAxis("Vertical");
+        Debug.Log("zInput: " + zInput);
 
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            // 오른쪽 방향키 입력이 감지된 경우 x 방향 힘가기
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
+        // 실제 이동 속도를 입력 값과 이동 속력을 통해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘가기
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        // Vector3 속도를 (xSpeed, 0, zSpeed)으로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        // 리지드바디의 속도에 newVelocity를 할당
+        playerRigidbody.linearVelocity = newVelocity;
+
+        // if (Input.GetKey(KeyCode.UpArrow) == true)
+        // {
+        //     // 위쪽 방향키 입력이 감지된 경우 z 방향 힘가기
+        //     playerRigidbody.AddForce(0f, 0f, speed);
+        // }
+
+        // if (Input.GetKey(KeyCode.DownArrow) == true)
+        // {
+        //     // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘가기
+        //     playerRigidbody.AddForce(0f, 0f, -speed);
+        // }
+
+        // if (Input.GetKey(KeyCode.RightArrow) == true)
+        // {
+        //     // 오른쪽 방향키 입력이 감지된 경우 x 방향 힘가기
+        //     playerRigidbody.AddForce(speed, 0f, 0f);
+        // }
+
+        // if (Input.GetKey(KeyCode.LeftArrow) == true)
+        // {
+        //     // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘가기
+        //     playerRigidbody.AddForce(-speed, 0f, 0f);
+        // }
     }
 
     public void Die()
